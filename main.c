ded1d4527a6181a14266ba242c97c31950aeed0e
Nik <nikrom17@bitbucket.org>
2018-06-04 22:06:44 -0700

    fix mistakes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/25 10:48:33 by nroman            #+#    #+#             */
-/*   Updated: 2018/06/04 21:59:18 by nroman           ###   ########.fr       */
+/*   Updated: 2018/06/04 22:06:21 by nroman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 
 int		main(void) 
 {
-	unsigned int		num = 42;
+	//unsigned int		num = 42;
+	wchar_t		c = 128;
 	//char	*str = "did";
 	//char	*str1 = "it";
 	//char	*str2 = "on `em";
 
-	//ft_printf("MEEE: %4.15d\n", num);
-	printf("THEM: %4.15d\n", num);
+	ft_printf("MEEE: %C\n", c);
+	printf("THEM: %C\n", c);
 	//ft_printf("MEEE - String value: %c %s %s\n",str[0], str1, str2);
 	//printf("THEM - String value: %c %s %s\n",str[0], str1, str2);
 
