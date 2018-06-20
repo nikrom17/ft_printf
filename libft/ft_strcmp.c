36f2c72d90d9a3462b64a7ef3ea1e3e71e36bf12
Nik <nikrom17@bitbucket.org>
2018-06-20 16:00:13 -0700

    fix mistakes

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 10:03:10 by nroman            #+#    #+#             */
-/*   Updated: 2018/02/27 20:06:44 by nroman           ###   ########.fr       */
+/*   Updated: 2018/06/20 15:59:58 by nroman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,15 @@ int		ft_strcmp(const char *s1, const char *s2)
 	unsigned int i;
 
 	i = 0;
-	while (s1[i])
+	if (s1 && s2)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		i++;
+		while (s1[i])
+		{
+			if ((unsigned char)s1[i] != (unsigned char)s2[i])
+				return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			i++;
+		}
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (1);
 }
