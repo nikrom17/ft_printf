187f41ca8b502122662e39444c19613217e14232
Nik <nikrom17@bitbucket.org>
2018-06-21 10:54:06 -0700

    fix mistakes

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nroman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/20 19:07:33 by nroman            #+#    #+#             */
-/*   Updated: 2018/06/21 10:53:32 by nroman           ###   ########.fr       */
+/*   Updated: 2018/06/21 10:54:02 by nroman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -153,7 +153,7 @@ void		reset_struct(t_struct *flags)
 	flags->type = '0';
 	flags->size_modifier = '0';
 	flags->neg = '0';
-	free(flags->str_args);
+	//free(flags->str_args);
 	flags->str_args = (char *)ft_memalloc(sizeof(wchar_t) * 2);
 	//ft_strclr(flags->str_args);
 	//flags->str_args[0] = 0;
